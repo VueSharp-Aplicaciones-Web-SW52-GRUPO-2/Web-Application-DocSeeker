023c6371d7199fda59223bc7a7b0ab9ef5d12b8d
Rafael <rafael.urquiaga@gmail.com>
2023-04-30 19:59:41 -0500

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+// Routing Module
+
+import { createRouter, createWebHistory } from "vue-router";
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+
+    ]
+});
+
+export default router;
