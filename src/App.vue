8dd1a2b71d384a38f89cdaa7baabba0d2c6ebf17
Rafael <rafael.urquiaga@gmail.com>
2023-05-01 12:04:11 -0500

    feat(): Add toolbar and sidebar template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
+    <header>
+        <pv-toolbar class="bg-primary">
+            <template #start>
+                <pv-button class="p-button-text text-white" icon="pi pi-bars"
+                           @click="sidebarVisible = !sidebarVisible"></pv-button>
+                <h3>DocSeeker</h3>
+            </template>
+            <template #end>
+
+            </template>
+        </pv-toolbar>
+    </header>
+    <pv-sidebar v-model:visible="sidebarVisible"></pv-sidebar>
 
 </template>
 
-<script >
+<script>
+import MainContent from "./docseeker/pages/main-content.component.vue";
+
 export default {
-    name: "App"
+    name: "App",
+    components: {MainContent},
+    data() {
+        return {
+            sidebarVisible: false
+        }
+    }
 }
 </script>
 
