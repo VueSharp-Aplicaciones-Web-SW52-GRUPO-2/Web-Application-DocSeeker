c1cc9cd92cb3e157ce702385f9c56251f5328c2f
Erick Armando Cueva Elera <89040344+ECueva1809@users.noreply.github.com>
2023-05-02 11:12:42 -0500

    Feat (pathologicals-list)
    Add pathologicals-list

---
+++ src/docseeker/pages/pathologicals-list.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script>
+import PathologicalItem from "./pathological-item.vue";
+
+export default {
+    name: "pathologicals-list",
+    components: {PathologicalItem},
+    props:["pathologicals"]
+}
+</script>
+
+<style scoped>
+
+</style>
