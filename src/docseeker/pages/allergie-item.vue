0d1327995b5b8ccea44b1af26046900379c21db3
Erick Armando Cueva Elera <89040344+ECueva1809@users.noreply.github.com>
2023-05-02 11:08:26 -0500

    Feat (allergie-item)
    Add allergie-iteam

---
+++ src/docseeker/pages/allergie-item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div><p>{{allergie.title}}</p></div>
+</template>
+
+<script>
+export default {
+    name: "allergie-item",
+    props:[
+        "allergie"
+    ]
+}
+</script>
+
+<style>
+</style>
