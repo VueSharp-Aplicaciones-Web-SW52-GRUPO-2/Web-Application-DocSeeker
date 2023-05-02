9d526a3887bf8a7dc7ff1272545070f1ee61d1e1
Rafael <rafael.urquiaga@gmail.com>
2023-05-02 01:50:25 -0500

    feat(router): Add route by default

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 // Routing Module
 
 import { createRouter, createWebHistory } from "vue-router";
+import HomeComponent from "../publilc/pages/home.component.vue";
 
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-
+        {path:"/home", component:HomeComponent},
+        {path:"/", redirect:"/home"}
     ]
 });
 
