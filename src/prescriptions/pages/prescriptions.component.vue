c2ca3da6ddf006add4ebda64e0ba30095cfa591a
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-05-19 20:48:12 -0500

    feat(prescriptions): Add full data-table template

--- src/prescriptions/pages/prescriptions.component.vue
+++ src/prescriptions/pages/prescriptions.component.vue
@@ -1,30 +1,59 @@
 <template>
-  <div class="content-edges">
-      <div>
-          <h1>Prescription</h1>
-          <h1>Prescription</h1>
-      </div>
-      <div>
-          <data-table>
-              <header>
+  <div class="global-container">
+    <div class="title-section-container">
+      <h1>Prescription</h1>
+    </div>
+    <div class="data-table-container">
+      <pv-toolbar class="mb-4">
+        <template #start>
+          <pv-button label="Export" icon="pi pi-upload" severity="help"
+                     @click="exportCSV($event)"></pv-button>
+        </template>
+      </pv-toolbar>
 
-              </header>
-              <pv-column>
+      <data-table ref="dt" :value="prescriptions" v-model:selection="selectedPrescriptions"
+                  dataKey="id" :paginator="true" :rows="10" :filters="filters"
+                  paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+                  :rowsPerPageOptions="[5,10,25]"
+                  currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products">
+        <template #header>
+          <div class="flex flex-wrap gap-2 align-items-center justify-content-between">
+            <h4 class="m-0">Manage Prescriptions</h4>
+            <span class="p-input-icon-left">
+                <i class="pi pi-search"/>
+                <pv-input-text v-model="filters['global'].value"
+                               placeholder="Search..."></pv-input-text>
+            </span>
+          </div>
+        </template>
 
-              </pv-column>
-          </data-table>
-      </div>
+        <pv-column selectionMode="multiple" style="width: 3rem" :exportable="false"></pv-column>
+        <pv-column field="code" header="Code" sortable style="min-width:12rem"></pv-column>
+        <pv-column field="name" header="Name" sortable style="min-width:16rem"></pv-column>
+        <pv-column field="price" header="Price" sortable style="min-width:8rem">
+          <template #body="slotProps">
+            {{ formatCurrency(slotProps.data.price) }}
+          </template>
+        </pv-column>
+        <pv-column field="category" header="Category" sortable style="min-width:10rem"></pv-column>
+        <pv-column field="prescriptionStatus" header="Status" sortable style="min-width:12rem">
+          <template #body="slotProps">
+            <pv-tag :value="slotProps.data.prescriptionStatus"
+                    :severity="getStatusLabel(slotProps.data.prescriptionStatus)"></pv-tag>
+          </template>
+        </pv-column>
+      </data-table>
+    </div>
 
   </div>
 </template>
 
 <script>
-import {defineComponent} from 'vue'
+import {FilterMatchMode} from "primevue/api";
 
-export default defineComponent({
-    name: "prescriptions",
-    components: {PvColumn}
-})
+export default {
+  name: "prescriptions"
+}
 </script>
 
 <style scoped>
