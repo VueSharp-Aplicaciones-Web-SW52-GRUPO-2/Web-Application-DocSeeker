8a29275cba35a197562089e3112e5001361c5c00
u201817461 <u201817461@upc.edu.pe>
2023-05-26 21:44:14 -0500

    chore: delete project structure

--- src/docseeker/services/news-api.service.js
+++
@@ -1,7 +0,0 @@
-import http from '../../shared/services/http-common.js'
-
-export class NewsApiService {
-    getAll(){
-        return http.get('/news')
-    }
-}
--- src/prescriptions/services/prescriptions-api.service.js
+++
@@ -1,7 +0,0 @@
-import http from "../../shared/services/http-common.js"
-
-export class PrescriptionsApiService {
-    getAll() {
-        return http.get('/prescriptions');
-    }
-}
